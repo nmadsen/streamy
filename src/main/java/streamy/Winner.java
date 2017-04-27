acfdce761bb28f12a923ae3bdfaea7f0eb2ea1b7
Cam Morris <cameron.morris@familysearch.org>
2017-04-27 11:26:19 -0600

    removed stream code

--- src/main/java/streamy/Winner.java
+++ src/main/java/streamy/Winner.java
@@ -48,33 +48,23 @@ public class Winner {
 
   public static void main(String args[]) {
     // Filter and Map -
-    List<String> winnersOfToursLessThan3500km = tdfWinners
-        .stream()
-        .filter(d -> d.getLengthKm() < 3500) // Separate out Tours less than 3500km
-        .map(Winner::getName) // Get names of winners
-        .collect(toList()); // Return a list
+    List<String> winnersOfToursLessThan3500km = null; //tdfWinners.stream()
+    // cool streaming code here
     // Winners of Tours Less than 3500km - [Alberto Contador, Cadel Evans, Bradley Wiggins, Chris Froome, Chris Froome]
-
     System.out.println("Winners of Tours Less than 3500km - " + winnersOfToursLessThan3500km);
     assertThat(winnersOfToursLessThan3500km,
         contains("Alberto Contador", "Cadel Evans", "Bradley Wiggins", "Chris Froome", "Chris Froome"));
 
-    List<String> winnersOfToursGreaterThan3500km = tdfWinners
-        .stream()
-        .filter(d -> d.getLengthKm() >= 3500)
-        .map(Winner::getName)
-        .collect(toList());
+    List<String> winnersOfToursGreaterThan3500km = null; //tdfWinners.stream()
+    // cool streaming code here
     // Winners of Tours Greater than 3500km - [Óscar Pereiro, Alberto Contador, Carlos Sastre, Andy Schleck, Vincenzo Nibali, Chris Froome]
     System.out.println("Winners of Tours Greater than 3500km - " + winnersOfToursGreaterThan3500km);
     assertThat(winnersOfToursGreaterThan3500km,
         contains("Óscar Pereiro", "Alberto Contador", "Carlos Sastre", "Andy Schleck", "Vincenzo Nibali", "Chris Froome"));
 
     // limit -
-    List<Winner> winnerObjectsOfToursLessThan3500kmLimit2 = tdfWinners
-        .stream()
-        .filter(d -> d.getLengthKm() < 3500)
-        .limit(2)
-        .collect(toList());
+    List<Winner> winnerObjectsOfToursLessThan3500kmLimit2 = null; //tdfWinners.stream()
+    // cool streaming code here
     // winnerObjectsOfToursLessThan3500kmLimit2 [Alberto Contador, Cadel Evans]
     System.out.println("winnerObjectsOfToursLessThan3500kmLimit2 " + winnerObjectsOfToursLessThan3500kmLimit2);
     assertThat(winnerObjectsOfToursLessThan3500kmLimit2,
@@ -83,50 +73,35 @@ public class Winner {
             hasProperty("name", equalTo("Cadel Evans"))
         ));
 
-    List<String> firstTwoWinnersOfToursLessThan3500km = tdfWinners
-        .stream()
-        .filter(d -> d.getLengthKm() < 3500)
-        .map(Winner::getName)
-        .limit(2)
-        .collect(toList());
-
+    List<String> firstTwoWinnersOfToursLessThan3500km = null; //tdfWinners.stream()
+    // cool streaming code here
     assertThat(firstTwoWinnersOfToursLessThan3500km,
         contains("Alberto Contador", "Cadel Evans"));
 
     // filter by distinct
-    List<String> distinctTDFWinners = tdfWinners
-        .stream()
-        .map(Winner::getName)
-        .distinct()
-        .collect(toList());
+    List<String> distinctTDFWinners = null; //tdfWinners.stream()
+    // cool streaming code here
     System.out.println("distinctTDFWinners - " + distinctTDFWinners);
     assertThat(distinctTDFWinners,
         hasSize(8));
 
-    long numberOfDistinceWinners = tdfWinners
-        .stream()
-        .map(Winner::getName)
-        .distinct()
-        .count();
+    long numberOfDistinceWinners = 0; //tdfWinners.stream()
+    // cool streaming code here
     // numberOfDistinceWinners - 8
     System.out.println("numberOfDistinceWinners - " + numberOfDistinceWinners);
     assertThat(numberOfDistinceWinners, equalTo(8L));
 
     // skip records
-    List<Winner> skipFirst2Winners = tdfWinners
-        .stream()
-        .skip(2)
-        .collect(toList());
+    List<Winner> skipFirst2Winners = null; //tdfWinners.stream()
+    // cool streaming code here
     // skipEveryOtherTDFWinner - [Carlos Sastre, Alberto Contador, Andy Schleck, Cadel Evans, Bradley Wiggins, Chris Froome, Vincenzo Nibali, Chris Froome, Chris Froome]
     System.out.println("skipEveryOtherTDFWinner - " + skipFirst2Winners);
     assertThat(skipFirst2Winners,
         everyItem(not(in(Arrays.asList(tdfWinners.get(0), tdfWinners.get(1)))))
     );
 
-    List<String> mapWinnerYearNamesToList = tdfWinners
-        .stream()
-        .map(w -> w.getYear() + " - " + w.getName())
-        .collect(toList());
+    List<String> mapWinnerYearNamesToList = null; //tdfWinners.stream()
+    // cool streaming code here
     // mapWinnerYearNamesToList [2006 - Óscar Pereiro, 2007 - Alberto Contador, 2008 - Carlos Sastre, 2009 - Alberto Contador, 2010 - Andy Schleck, 2011 - Cadel Evans, 2012 - Bradley Wiggins, 2013 - Chris Froome, 2014 - Vincenzo Nibali, 2015 - Chris Froome, 2016 - Chris Froome]
     System.out.println("mapWinnerYearNamesToList " + mapWinnerYearNamesToList);
     assertThat(mapWinnerYearNamesToList,
@@ -135,11 +110,8 @@ public class Winner {
     ));
 
 
-    List<Integer> mapWinnerNameLengthToList = tdfWinners
-        .stream()
-        .map(Winner::getName)
-        .map(String::length)
-        .collect(toList());
+    List<Integer> mapWinnerNameLengthToList = null; //tdfWinners.stream()
+    // cool streaming code here
     // mapWinnerNameLengthToList [13, 16, 13, 16, 12, 11, 15, 12, 15, 12, 12]
     System.out.println("mapWinnerNameLengthToList " + mapWinnerNameLengthToList);
     assertThat(mapWinnerNameLengthToList,
@@ -147,59 +119,52 @@ public class Winner {
     ));
 
     // matching - allMatch, noneMatch
-    Optional<Winner> anyWinnerNamedWiggins = tdfWinners.stream()
-        .filter(w -> w.getName().contains("Wiggins"))
-        .findAny();
+    Optional<Winner> anyWinnerNamedWiggins = null; //tdfWinners.stream()
+    // cool streaming code here
     // winner2012 - Bradley Wiggins
     System.out.println("winner2012 - " + anyWinnerNamedWiggins.get());
     assertThat(anyWinnerNamedWiggins.get().getName(), containsString("Wiggins"));
 
-    Optional<Integer> firstWinnerOfYear2014 = tdfWinners.stream()
-        .map(Winner::getYear)
-        .filter(x -> x == 2014)
-        .findFirst();
+    Optional<Integer> firstWinnerOfYear2014 = null; //tdfWinners.stream()
+    // cool streaming code here
     // winnerYear2014 - 2014
     System.out.println("winnerYear2014 - " + firstWinnerOfYear2014.get());
     assertThat(firstWinnerOfYear2014.get(), equalTo(2014));
 
     // reducing - 0 --> initial value
-    int totalDistance = tdfWinners.stream()
-        .map(Winner::getLengthKm)
-        .reduce(0, Integer::sum);
+    int totalDistance = 0; //tdfWinners.stream()
+    // cool streaming code here
     // totalDistance - 38767
     System.out.println("totalDistance - " + totalDistance);
     assertThat(totalDistance, equalTo(38767));
 
-    Optional<Integer> shortestYear = tdfWinners.stream()
-        .map(Winner::getLengthKm)
-        .reduce(Integer::min);
+    Optional<Integer> shortestYear = null; //tdfWinners.stream()
+    // cool streaming code here
     // shortestYear - 3360
     System.out.println("shortestYear - " + shortestYear.get());
     assertThat(shortestYear.get(), equalTo(3360));
 
-    Optional<Integer> longestYear = tdfWinners.stream()
-        .map(Winner::getLengthKm)
-        .reduce(Integer::max);
+    Optional<Integer> longestYear = null; //tdfWinners.stream()
+    // cool streaming code here
     // longestYear - 3661
     System.out.println("longestYear - " + longestYear.get());
     assertThat(longestYear.get(), equalTo(3661));
 
-    Optional<Winner> fastestAveSpeedOfWinner = tdfWinners.stream()
-        .min(Comparator.comparingDouble(Winner::getAveSpeed));
+    Optional<Winner> fastestAveSpeedOfWinner = null; //tdfWinners.stream()
+    // cool streaming code here
     System.out.println("fastestTDF - " + fastestAveSpeedOfWinner.get());
     assertThat(fastestAveSpeedOfWinner.get().getAveSpeed(), closeTo(39, 0));
 
     // shorthand use mapToDouble
-    OptionalDouble fastestTDF = tdfWinners.stream()
-        .mapToDouble(Winner::getAveSpeed)
-        .min();
+    OptionalDouble fastestTDF = null; //tdfWinners.stream()
+    // cool streaming code here
     // fastestTDF - 39.0
     System.out.println("fastestTDF - " + fastestTDF.getAsDouble());
     assertThat(fastestTDF.getAsDouble(), closeTo(39, 0));
 
     // groupingby - make a map whose keys are names
-    Map<String, List<Winner>> namesVsWinner = tdfWinners.stream()
-        .collect(groupingBy(Winner::getName));
+    Map<String, List<Winner>> namesVsWinner = null; //tdfWinners.stream()
+    // cool streaming code here
     // namesVsWinner - {Bradley Wiggins=[Bradley Wiggins], Carlos Sastre=[Carlos Sastre], Cadel Evans=[Cadel Evans], Óscar Pereiro=[Óscar Pereiro], Chris Froome=[Chris Froome, Chris Froome, Chris Froome], Andy Schleck=[Andy Schleck], Alberto Contador=[Alberto Contador, Alberto Contador], Vincenzo Nibali=[Vincenzo Nibali]}
     System.out.println("namesVsWinner - " + namesVsWinner);
     assertThat(namesVsWinner, allOf(
@@ -208,16 +173,15 @@ public class Winner {
     ));
 
     // join strings
-    String allTDFWinnersTeamsCSV = tdfWinners.stream()
-        .map(Winner::getTeam)
-        .collect(joining(", "));
+    String allTDFWinnersTeamsCSV = null; //tdfWinners.stream()
+    // cool streaming code here
     // allTDFWinnersTeams Caisse d'Epargne–Illes Balears, Discovery Channel, Team CSC, Astana, Team Saxo Bank, BMC Racing Team, Team Sky, Team Sky, Astana, Team Sky, Team Sky
     System.out.println("allTDFWinnersTeams " + allTDFWinnersTeamsCSV);
     assertThat(allTDFWinnersTeamsCSV, equalTo("Caisse d'Epargne–Illes Balears, Discovery Channel, Team CSC, Astana, Team Saxo Bank, BMC Racing Team, Team Sky, Team Sky, Astana, Team Sky, Team Sky"));
 
     // grouping
-    Map<String, List<Winner>> winnersByNationality = tdfWinners.stream()
-        .collect(groupingBy(Winner::getNationality));
+    Map<String, List<Winner>> winnersByNationality = null; //tdfWinners.stream()
+    // cool streaming code here
     // winnersByNationality - {Great Britain=[Bradley Wiggins, Chris Froome, Chris Froome, Chris Froome], Luxembourg=[Andy Schleck], Italy=[Vincenzo Nibali], Australia=[Cadel Evans], Spain=[Óscar Pereiro, Alberto Contador, Carlos Sastre, Alberto Contador]}
     System.out.println("winnersByNationality - " + winnersByNationality);
     assertThat(winnersByNationality, allOf(
@@ -229,8 +193,8 @@ public class Winner {
         hasEntry(equalTo("Luxembourg"), hasSize(1))
     ));
 
-    Map<String, Long> winsByNationalityCounting = tdfWinners.stream()
-        .collect(groupingBy(Winner::getNationality, counting()));
+    Map<String, Long> winsByNationalityCounting = null; //tdfWinners.stream()
+    // cool streaming code here
     // winsByNationalityCounting - {Great Britain=4, Luxembourg=1, Italy=1, Australia=1, Spain=4}
     System.out.println("winsByNationalityCounting - " + winsByNationalityCounting);
     assertThat(winsByNationalityCounting, allOf(
